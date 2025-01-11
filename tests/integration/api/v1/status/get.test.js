add5600749e59ffa5d02b947526228df43635eb4
RubioCosta <rubioc02@gmail.com>
2025-01-11 02:06:35 +0000

    build: install `swr`

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -13,7 +13,7 @@ describe("GET /api/v1/status", () => {
       const responseBody = await response.json();
 
       const parsedUpdateAt = new Date(responseBody.update_at).toISOString();
-      expect(responseBody.update_at).toEqual(parsedUpdateAt);
+      expect(responseBody.updated_at).toEqual(parsedUpdateAt);
 
       expect(responseBody.dependencies.database.version).toEqual("16.0");
       expect(responseBody.dependencies.database.max_connections).toEqual(100);
